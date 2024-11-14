5a5d3d635c81af125e3581308bd2077881e4add4
Ashton Hotke <339741886@GoTVDSB.ca>
2024-11-14 09:23:26 -0500

    Add project files.

---
+++ Topic 4 - Timers and Sound/Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Topic_4___Timers_and_Sound
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D bombTexture, snipsTexture;
+        SoundEffect explode;
+        Rectangle bombRect;
+
+        SpriteFont timeFont;
+
+        float seconds = 15;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here-----------
+
+            base.Initialize();
+            bombRect = new Rectangle(50, 50, 700, 400);
+            
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            bombTexture = Content.Load<Texture2D>("bomb");
+            timeFont = Content.Load<SpriteFont>("TimeFont");
+            explode = Content.Load<SoundEffect>("explosion");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            seconds -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (seconds < 0)
+                explode.Play();
+                seconds = 15;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+            _spriteBatch.DrawString(timeFont, seconds.ToString("00.0"), new Vector2(290, 210), Color.DarkRed);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Topic 4 - Timers and Sound/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Topic_4___Timers_and_Sound.Game1();
+game.Run();
